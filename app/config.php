eb207e76a256b72db69f6cf9295f6c602f8e3a30
Gustav Trenwith <gtrenwith@gmail.com>
2015-11-12 08:51:47 +0200

    Updated config

--- app/config.php
+++ app/config.php
@@ -7,11 +7,5 @@ date_default_timezone_set('Africa/Johannesburg');
 define('APP_PATH', __DIR__.'/');
 
 # Set the URL to be used in the templates and views
-define('PUBLIC_PATH', 'swordfish.goosephp.com/public/');
-define('URL', 'http://l/'.PUBLIC_PATH);
-
-# Set GitHub variables
-define('GITHUB_ID', '06d43c835d60e96f553c');
-define('GITHUB_SECRET', '62cec94853c7d641090f62a0116de062974e0b0d');
-define('GITHUB_OWNER', 'WorkAtSwordfish');
-define('GITHUB_REPO', 'GitIntegration');
+define('PUBLIC_PATH', 'example.com');
+define('URL', 'http://'.PUBLIC_PATH);
